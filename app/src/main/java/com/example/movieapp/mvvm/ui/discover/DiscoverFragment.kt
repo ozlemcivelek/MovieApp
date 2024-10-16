a1ff61e1126b3cacee7eb50fe9206a4bdfc976f5
Ozlem Civelek Bayraktar <cvlk.ozlem54@gmail.com>
2024-10-16 16:05:40 +0300

    Discover Fragment -> Rewritten with ViewModel

---
+++ app/src/main/java/com/example/movieapp/mvvm/ui/discover/DiscoverFragment.kt
@@ -0,0 +1,76 @@
+package com.example.movieapp.mvvm.ui.discover
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.example.movieapp.DiscoverScreenFragmentArgs
+import com.example.movieapp.DiscoverScreenFragmentDirections
+import com.example.movieapp.R
+import com.example.movieapp.adapter.RecyclerViewAdapter
+import com.example.movieapp.databinding.FragmentDetailScreenBinding
+import com.example.movieapp.databinding.FragmentDiscoverScreenBinding
+import com.example.movieapp.mvvm.ui.dashboard.DashboardFragmentDirections
+import com.example.movieapp.mvvm.ui.dashboard.DashboardViewModel
+import com.example.movieapp.mvvm.ui.detail.DetailFragmentArgs
+
+class DiscoverFragment : Fragment() {
+
+    private var _binding: FragmentDiscoverScreenBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel by viewModels<DiscoverViewModel>()
+    private val args by navArgs<DiscoverFragmentArgs>()
+
+    private var recyclerViewAdapter = RecyclerViewAdapter()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentDiscoverScreenBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.discoverScreenBackBtn.setOnClickListener{
+            findNavController().popBackStack()
+        }
+
+        binding.discoverScreenCloseBtn.setOnClickListener{
+            val action = DiscoverFragmentDirections.actionDiscoverFragmentToDashboardFragment()
+           findNavController().navigate(action)
+        }
+
+        recyclerViewAdapter.onItemClicked = { itemId ->
+            Log.d("TAG", "onResponse: Discover Item Clicked")
+            val action = DiscoverFragmentDirections.actionDiscoverFragmentToDetailFragment(itemId)
+            view?.findNavController()?.navigate(action)
+        }
+        binding.discoverScreenRecyclerView.adapter = recyclerViewAdapter
+
+        viewModel.topDiscoverMovies(args.genresId)
+        observeAndHandleTopRatedMovies()
+    }
+
+    private fun observeAndHandleTopRatedMovies() {
+        viewModel.discoverMovies.observe(viewLifecycleOwner) { movies ->
+            if (movies.isEmpty()) return@observe
+            recyclerViewAdapter.setItems(movies)
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/movieapp/mvvm/ui/discover/DiscoverViewModel.kt
@@ -0,0 +1,44 @@
+package com.example.movieapp.mvvm.ui.discover
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.navigation.findNavController
+import com.example.movieapp.DiscoverScreenFragmentDirections
+import com.example.movieapp.adapter.RecyclerViewAdapter
+import com.example.movieapp.models.MovieModel
+import com.example.movieapp.models.TopRatedMovieResponse
+import com.example.movieapp.network.retrofit.Retrofit
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class DiscoverViewModel : ViewModel() {
+
+    private val service = Retrofit.getMovieService()
+
+    val discoverMovies: MutableLiveData<List<MovieModel>> = MutableLiveData(emptyList())
+
+    fun topDiscoverMovies(genresId: String ){
+        service.getDiscoverMovies(genresId).enqueue(object: Callback<TopRatedMovieResponse> {
+            override fun onResponse(
+                call: Call<TopRatedMovieResponse>,
+                response: Response<TopRatedMovieResponse>
+            ) {
+
+                if (response.isSuccessful) {
+                    response.body()?.results?.let { safeResult ->
+                        discoverMovies.value = safeResult
+                    }
+                }
+            }
+            override fun onFailure(call: Call<TopRatedMovieResponse>, t: Throwable) {
+                Log.e("TAG", "onFailure: ${t.message}", t)
+            }
+
+        })
+    }
+
+
+
+}
